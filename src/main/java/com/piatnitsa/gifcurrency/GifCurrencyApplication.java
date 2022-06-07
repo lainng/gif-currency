3af620ec526bef411be1225b103f72576a128924
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-06-07 16:44:43 +0300

    Initial commit

---
+++ src/main/java/com/piatnitsa/gifcurrency/GifCurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.piatnitsa.gifcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifCurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GifCurrencyApplication.class, args);
+    }
+
+}
