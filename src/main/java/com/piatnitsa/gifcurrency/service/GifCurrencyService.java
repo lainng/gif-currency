e7b2803d943c2ac14228efb341e35b0967344a20
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-06-10 12:24:12 +0300

    Code refactoring & logic changing

--- src/main/java/com/piatnitsa/gifcurrency/service/GifCurrencyService.java
+++ src/main/java/com/piatnitsa/gifcurrency/service/GifCurrencyService.java
@@ -1,41 +1,51 @@
 package com.piatnitsa.gifcurrency.service;
 
-import com.piatnitsa.gifcurrency.feign.ExchangeClient;
-import com.piatnitsa.gifcurrency.feign.GifClient;
 import com.piatnitsa.gifcurrency.model.ExchangeRate;
 import com.piatnitsa.gifcurrency.model.Gif;
-import com.piatnitsa.gifcurrency.dto.GifDto;
+import com.piatnitsa.gifcurrency.util.ExchangeRatesComparator;
+import com.piatnitsa.gifcurrency.validator.RequestParameterValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
 
 @Service
 public class GifCurrencyService {
-    private final ExchangeClient exchangeClient;
-    private final GifClient gifClient;
+
+    @Value("${richTag}") private String richTag;
+    @Value("${brokeTag}") private String brokeTag;
+    @Value("${sameRatesTag}") private String sameTag;
+    private final ExchangeRatesComparator ratesComparator;
+    private final ExchangeRateService exchangeRateService;
+    private final GifService gifService;
 
     @Autowired
-    public GifCurrencyService(ExchangeClient exchangeClient, GifClient gifClient) {
-        this.exchangeClient = exchangeClient;
-        this.gifClient = gifClient;
+    public GifCurrencyService(ExchangeRatesComparator ratesComparator,
+                              ExchangeRateService exchangeRateService,
+                              GifService gifService) {
+        this.ratesComparator = ratesComparator;
+        this.exchangeRateService = exchangeRateService;
+        this.gifService = gifService;
     }
 
-    public ExchangeRate getExchangeRates(String appId, String currencyCode) {
-        return exchangeClient.getLatestExchangeRates(appId, currencyCode).getBody();
-    }
-
-    public Gif getGif(String appId, String tag) {
-        GifDto gifDto = gifClient.getRandomGif(appId, tag);
-        Gif gif = toEntity(gifDto);
-        gif.setTag(tag);
-        return gif;
-    }
-
-    private Gif toEntity(GifDto dto) {
-        Gif gif = new Gif();
-        Map<String, Object> data = dto.getGifData();
-        gif.setUrl((String) data.get("url"));
+    public Gif gifByRate(String currencyCode) {
+        RequestParameterValidator.validateCurrencyCode(currencyCode);
+        ExchangeRate currentRate = exchangeRateService.getLatestExchangeRate(currencyCode);
+        ExchangeRate yesterdayRate = exchangeRateService.getYesterdayRate(currencyCode);
+        int comparingValue = ratesComparator.compare(currentRate, yesterdayRate);
+        Gif gif;
+        switch (comparingValue) {
+            case 1: {
+                gif = gifService.getGifByTag(richTag);
+                break;
+            }
+            case -1: {
+                gif = gifService.getGifByTag(brokeTag);
+                break;
+            }
+            default: {
+                gif = gifService.getGifByTag(sameTag);
+            }
+        }
         return gif;
     }
 }
